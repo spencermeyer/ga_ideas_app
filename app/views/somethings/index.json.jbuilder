33bb786c17835f989debab53b00166bc8ed6bf95
spencermeyer <spencer_meyer@hotmail.com>
2015-05-04 13:29:22 +0100

    unscaffolded somethings

--- app/views/somethings/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@somethings) do |something|
-  json.extract! something, :id
-  json.url something_url(something, format: :json)
-end
--- db/migrate/20150504110956_create_somethings.rb
+++
@@ -1,8 +0,0 @@
-class CreateSomethings < ActiveRecord::Migration
-  def change
-    create_table :somethings do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
