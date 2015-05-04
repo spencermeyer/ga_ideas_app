8d8c010455537877199db8f4ddbcc5ed6863efc2
spencermeyer <spencer_meyer@hotmail.com>
2015-05-04 17:11:21 +0100

    resolved conflict in medium model

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,17 +1,8 @@
 class Medium < ActiveRecord::Base
-<<<<<<< HEAD
-  belongs_to :idea
-  validates :idea_id, presence: true
-  validates :idea_id, uniqueness: true
-  validates :title, presence: true
-  validates :media_type, presence: true
-  validates :content, presence: true
-=======
 belongs_to :idea
 validates :idea_id, presence: true
 validates :idea_id, uniqueness: true
 validates :title, presence: true, length: { in: 2..50, message: "Title should be 2-50 letters"}
 validates :media_type, presence: true
 validates :content, presence: true
->>>>>>> 770a72d77ffd8c5b819fb0ffbe3c62c5e69770aa
 end
