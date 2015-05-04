c7f036a40b4c839394b0b4f452a1f743789509bd
pipo24 <filippo.sm.matoso@gmail.com>
2015-05-04 15:39:17 +0100

    tested validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ActiveRecord::Base
   validates :first_name,
      presence: true,
      length: { in: 2..20},
-     format: { with: /\A[a-zA-Z]+\z/, message: "First name should only contain letters" },
+     format: { with: /\Aron[a-zA-Z]+\z/, message: "First name should only contain letters" }
   validates :last_name,
      presence: true,
      length: { in: 2..50},
-     format: { with: /\A[a-zA-Z]+\z/, message: "Last name should only contain letters" },
+     format: { with: /\A[a-zA-Z]+\z/, message: "Last name should only contain letters" }
   validates :dob,
      presence: true
   validates :role,
