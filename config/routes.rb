401d3ae3ab1c55d51b6584ef82bdbde371b5aefd
danizraikat <danizraikat@gmail.com>
2015-05-07 13:25:17 +0100

    edited nav bar...missing ga logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  
   resources :users, only:[:index, :show]
   resources :ideas
 
@@ -11,19 +12,14 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :comments, only:[:create, :destroy]
-<<<<<<< HEAD
-=======
-  
-  resources :media
-
-  # , only:[:create, :edit, :show, :new, :update]
-
   resources :ideas do
     resources :media
   end
 
   resources :comments, only:[:create, :destroy]
+  
+  resources :media
 
->>>>>>> 4a9444dc97fbbe174e31dd6e24e86216a31d3936
+  # only:[:create, :edit, :show, :new, :update]
+
 end
