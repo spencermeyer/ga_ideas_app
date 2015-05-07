a0aa47d7b42f902ba3141cf30c631c629e7f5f4f
spencermeyer <spencer_meyer@hotmail.com>
2015-05-07 19:40:33 +0100

    added seeds and did lots of form width adjustment

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -78,7 +78,7 @@ class IdeasController < ApplicationController
   # POST /ideas
   # POST /ideas.json
   def create
-    @idea = Idea.new(idea_params)
+    @idea = current_user.ideas.new(idea_params)
 
     respond_to do |format|
       if @idea.save
